d7c4e57d2b11bb6d7489a693c26093c9869e4357
crcrpar <masaki.kozuki.2014@gmail.com>
2019-07-20 15:31:26 +0900

    remove some modules

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,21 +9,11 @@ let package = Package(
         .executable(name: "MNIST", targets: ["MNIST"]),
         .executable(name: "CIFAR", targets: ["CIFAR"]),
         .executable(name: "ResNet", targets: ["ResNet"]),
-        .executable(name: "MiniGoDemo", targets: ["MiniGoDemo"]),
-        .library(name: "MiniGo", targets: ["MiniGo"]),
     ],
     targets: [
-        .target(name: "Autoencoder", path: "Autoencoder"),
         .target(name: "CIFAR", path: "CIFAR"),
         .target(name: "Catch", path: "Catch"),
-        .target(name: "Gym-FrozenLake", path: "Gym/FrozenLake"),
-        .target(name: "Gym-CartPole", path: "Gym/CartPole"),
-        .target(name: "Gym-Blackjack", path: "Gym/Blackjack"),
         .target(name: "MNIST", path: "MNIST"),
-        .target(name: "MiniGo", path: "MiniGo", exclude: ["main.swift"]),
-        .target(name: "MiniGoDemo", dependencies: ["MiniGo"], path: "MiniGo",
-                sources: ["main.swift"]),
-        .testTarget(name: "MiniGoTests", dependencies: ["MiniGo"]),
         .target(name: "ResNet", path: "ResNet"),
         .target(name: "Transformer", path: "Transformer"),
     ]
